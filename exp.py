16525c3496ad52f2748b017c84c2983d54f3f4d6
Dino Source <dinosource.cpp@gmail.com>
2024-04-28 18:39:10 -0700

    Add hashtables naive implementation (Grokking algs, ch. 5, ex. 4)

--- exp.py
+++
@@ -1,6 +0,0 @@
-def move_zeroes_to_the_end(numbers: list[int]) -> list[int]:
-    numbers.sort(key=lambda n: n==0)
-    return numbers
-
-x = [0, 1, -9, 4, 0, 6, -5, 0, 2, 3, 0]
-print(move_zeroes_to_the_end(x))
---
+++ hashtables.py
@@ -0,0 +1,45 @@
+def map_chars_to_prime_numbers():
+    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+             ' ',]
+    pnums = [  2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41,
+              43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101,
+             103, 107, 109, 113, 127, 131, 139, 149, 151, 157, 163, 167, 173,
+             179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241,
+             251,]
+    return dict(zip(chars, pnums))
+
+
+def hashfunc(name: str) -> int:
+    abc = map_chars_to_prime_numbers()
+    sum = 0
+    for ch in name:
+        sum += abc[ch]
+    return sum % len(name)
+
+names = ["John Smith", "Jane Smith", "Will Smith", "Kent Beck", "Rick Crow", "Dan Brown", "Adam Smith", "Ian Paice", "Ian Gillan"]
+
+
+def fill_hashtable(names: list) -> list:
+    my_hashtable = [[],] * 16
+    for name in names:
+        index = hashfunc(name)
+        if not my_hashtable[index]:
+            my_hashtable[index] = [name, ]
+        else:
+            my_hashtable[index].append(name)
+    return my_hashtable
+
+
+def main():
+    h = fill_hashtable(names)
+    i = 0
+    for bucket in h:
+        print(f"{i}. {bucket}")
+        i += 1
+
+
+main()
+
