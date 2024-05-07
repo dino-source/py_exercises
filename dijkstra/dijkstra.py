691a8c9f71e8de2f5f749745a8db8e5c507531e7
Dino Source <dinosource.cpp@gmail.com>
2024-05-06 17:46:10 -0700

    Add one more exercise for Dijkstra search

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -1,22 +1,24 @@
 from dijkstra_search import find_lowest_cost_node
-from globals import costs, graph, parents, processed
+from globals_1 import costs, graph, parents, processed
 
 
-node = find_lowest_cost_node(costs, processed)
-
-while node is not None:
-    cost = costs[node]
-    neighbors = graph[node]
-    for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
-        if costs[n] > new_cost:
-            costs[n] = new_cost
-            parents[n] = node
-    processed.add(node)
+def main():
     node = find_lowest_cost_node(costs, processed)
 
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.add(node)
+        node = find_lowest_cost_node(costs, processed)
 
+    print(f"The shortest path from start to fin is {costs['fin']}")
 
+main()
 
 
 
---
+++ dijkstra/globals_2.py
@@ -0,0 +1,38 @@
+from math import inf as infinity
+
+
+graph = {}
+graph['start'] = {}
+graph['start']['a'] = 7
+graph['start']['b'] = 2
+
+graph['a'] = {}
+graph['a']['c'] = 4
+graph['a']['d'] = 2
+
+graph['b'] = {}
+graph['b']['a'] = 8
+graph['b']['d'] = 7
+
+graph['c']['d'] = 6
+graph['c']['fin'] = 3
+
+graph['d']['fin'] = 1
+
+graph['fin'] = {}
+
+costs = {}
+costs['a'] = 5
+costs['b'] = 2
+costs['c'] = infinity
+costs['d'] = infinity
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['c'] = None
+parents['d'] = None
+parents['fin'] = None
+
+processed = set()
